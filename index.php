5a53e31c9421858b4e2329678918b548606468fe
Lucas Coelho <lucascoelho2211@gmail.com>
2026-03-25 15:48:10 +0100

    partie 2 finie

--- index.php
+++ index.php
@@ -1,28 +1,49 @@
 <?php
 
-require(__DIR__."/Model/pdo.php");
+    require(__DIR__."/Model/pdo.php");
 
-$res = $dbPDO->prepare("SELECT * FROM eleves");
-$res->execute();
+    $res = $dbPDO->prepare("SELECT * FROM eleves");
+    $res->execute();
+    $clients = $res->fetchAll();
 
-//echo "<br><br>";
+    //echo "<pre>";
+    //var_dump($clients);
 
-$clients = $res->fetchAll();
+    echo "Voici les noms qui sont présents dans la classe :<br><br>";
+    foreach($clients as $e) {
+        echo "<li>".$e['nom']." ".$e['prenom'];
+    }
 
-echo "<pre>";
+    echo "<br>";
 
-//var_dump($clients);
+    $res = $dbPDO->prepare("SELECT * FROM classes");
+    $res->execute();
+    $clients = $res->fetchAll();
 
-foreach($clients as $client) {
-    echo "<li>".$client['nom'];
-}
-/*
-$rows = count($clients);
+    echo "<br>Voici la liste de toute les classes :<br><br>";
 
-if ($rows > 0){
-    echo "Il y a $rows client(s)";
-} else {
-    echo "Aucun client";
-}*/
+    foreach($clients as $e) {
+        echo "<li>".$e['nom'];
+    }
+
+    $res = $dbPDO->prepare("SELECT * FROM prof");
+    $res->execute();
+    $clients = $res->fetchAll();
+
+    echo "<br><br>Voici la liste de tout les profs :<br><br>";
+
+    foreach($clients as $e) {
+        echo "<li>".$e['nom']." ".$e['prenom'];
+    }
+
+    $res = $dbPDO->prepare("SELECT p.nom AS nomProf, p.prenom AS prenomProf, c.nom AS nomClasse, m.nom AS nomMatiere FROM prof as p JOIN asso_3 AS a ON a.Id_Prof = p.Id_Prof JOIN classes AS c ON c.Id_Classes = a.Id_Classes JOIN matières AS m ON m.Id_Prof = p.Id_Prof;");
+    $res->execute();
+    $clients = $res->fetchAll();
+
+    echo "<br><br>Voici la liste de tout les profs et de leur classes et matières:<br><br>";
+
+    foreach($clients as $e) {
+        echo "<li>".$e['nomProf']." ".$e['prenomProf']." prof de ".$e['nomMatiere']." pour la classe de ".$e['nomClasse'];
+    }
 
 ?>
